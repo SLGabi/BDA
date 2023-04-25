1b23282a76166e246dc143870ce72bb99fe58688
SLGabi <gdsilvalucas@gmail.com>
2023-04-25 20:31:37 -0300

    api

--- API/routes/nodo.js
+++ API/routes/nodo.js
@@ -1,7 +1,9 @@
 module.exports = app => {
     const controller = require('../controllers/nodo.js')();
   
-    app.route('/API/v1/nodo')
+    app.route('/API/v1/listUser')
       .get(controller.listUser)
+    
+    app.route('API/v1/createUser')
       .post(controller.createUser);
   }
